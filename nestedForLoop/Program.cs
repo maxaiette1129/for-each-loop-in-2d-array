cb3e0286322227122927b30092b1fdce70c75e6d
maxai <maxai@MAX>
2023-04-17 21:59:46 +0800

    Add project files.

---
+++ nestedForLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿string[,] user =
+{
+    {"Max","max12310" },
+    {"glezel","glezel12310" },
+    {"ralph","ralph12310" },
+    {"nina","nina12310" },
+    {"andrea","andrea12310" },
+    {"power","power12310" },
+    {"hyper","hyer12310" },
+    {"egul","egul12310" },
+};
+
+int i = 1;
+foreach(string j in user)
+{
+    Console.WriteLine(j);
+    if (i % user.GetLength(1) == 0) Console.WriteLine();
+    i++;
+}
